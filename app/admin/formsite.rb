fdb4f951a42960e110475f9d2fe7730a2ed9c802
gofreegan <surkovsc@gmail.com>
2018-07-03 21:06:59 +0400

    add actions to formsite admin index page

--- app/admin/formsite.rb
+++ app/admin/formsite.rb
@@ -9,6 +9,7 @@ ActiveAdmin.register Formsite do
     column :droplet_ip
     column :zone_id
     column :created_at
+    actions
   end
 
   form do |f|
@@ -20,7 +21,6 @@ ActiveAdmin.register Formsite do
     f.actions
   end
 
-  # TODO change website to formsite
   action_item :setup, :only => :show do
     link_to setup_api_v1_formsites_path(formsite), class: 'button', remote: true do
       'Setup Formsite'
