89f9d3681511f91d4cbdbbe0efbf472cbe8e0075
gofreegan <surkovsc@gmail.com>
2018-07-13 13:24:29 +0300

    add ad to formsite serializer

---
+++ app/serializers/formsite_serializer.rb
@@ -0,0 +1,4 @@
+class FormsiteSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :droplet_id, :droplet_ip, :zone_id, :created_at, :updated_at, :repo_url, :ad_client, :ad_sidebar_id, :ad_top_id, :ad_middle_id, :ad_bottom_id, :first_redirect_url, :final_redirect_url, :favicon_image, :logo_image, :is_thankyou, :background, :left_side_content, :first_question_code_snippet, :head_code_snippet, :is_checkboxes
+  has_many :ads
+end
