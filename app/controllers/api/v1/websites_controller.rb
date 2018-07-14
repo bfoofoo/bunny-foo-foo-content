aa0c4b4bc520858c800f0bb676d47d6a81fd48c4
gofreegan <surkovsc@gmail.com>
2018-07-14 13:30:43 +0300

    update websites controller

--- app/controllers/api/v1/websites_controller.rb
+++ app/controllers/api/v1/websites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::WebsitesController < ApiController
   before_action :set_website, only: [
-      :show, :get_categories, :get_articles,
+      :show, :get_categories, :get_articles, :get_product_cards,
       :get_category_with_articles, :get_category_article,
       :setup, :build, :rebuild_old, :get_config
   ]
