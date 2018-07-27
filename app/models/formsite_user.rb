8e9f651e6d2a7f181c035b18d20846030bbc9fc5
Dendos <cotoha92@gmail.com>
2018-07-27 17:58:30 +0300

    Add scope to formsite users

--- app/models/formsite_user.rb
+++ app/models/formsite_user.rb
@@ -3,4 +3,10 @@ class FormsiteUser < ApplicationRecord
   belongs_to :user
 
   delegate :email, to: :user, allow_nil: true
+
+  scope :by_s_filter, -> (s_field) { 
+    where.not("#{s_field}" => nil)
+    .where.not("#{s_field}" => "")
+  }
+
 end
