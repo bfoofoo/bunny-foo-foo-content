596d73e59581d379d8b3c890f7d84864d338b2b7
Dendos <cotoha92@gmail.com>
2018-08-01 01:20:59 +0300

    Add decorator for froala editor config

---
+++ app/decorators/formsite_decorator.rb
@@ -0,0 +1,14 @@
+class FormsiteDecorator < Draper::Decorator
+  delegate_all
+
+  def admin_contet_wysiwyg_config
+    {
+      data: {
+        options: {
+          toolbarButtons: ['undo', 'redo', 'bold', 'italic', 'underline', 'color', 'insertLink','fontFamily', 'fontSize', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote'],
+          quickInsertButtons: ['table', 'ul', 'ol', 'hr']
+        }
+      }
+    }
+  end
+end
