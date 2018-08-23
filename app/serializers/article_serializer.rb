b90bf77e771e0141ba46589661a7d27092144692
Dendos <cotoha92@gmail.com>
2018-08-23 17:29:13 +0300

    remove websites from articles and categories

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,4 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :name, :content, :short, :slug, :cover_image
-  belongs_to :category, serializer: CategorySerializer
+  # belongs_to :category, serializer: CategorySerializer
 end
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,6 +1,6 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :slug
-  has_many :websites
-  has_many :formsites
+  # has_many :websites
+  # has_many :formsites
   # has_and_belongs_to_many :website, serializer: WebsiteSerializer
 end
