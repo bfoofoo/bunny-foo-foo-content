8e121356f00ff68641054175fd9268ed9b6b5eb4
Egor Romanov <unsaidxpl@gmail.com>
2018-08-28 14:58:47 +0300

    allow to refresh maropost account lists

--- app/admin/maropost_account.rb
+++ app/admin/maropost_account.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register MaropostAccount do
   permit_params :name, :account_id, :auth_token
 
+  member_action :refresh_list, method: :post do
+    account = MaropostAccount.find(params[:id])
+    MaropostInteractor::RefreshAccountLists.call(account_id: account.id)
+    redirect_to admin_maropost_account_path(account)
+  end
+
   index do
     column :id
     column :name
@@ -40,5 +46,18 @@ ActiveAdmin.register MaropostAccount do
     end
 
     f.actions
+  end
+
+
+  action_item :add_account, :only => [:show] do
+    link_to refresh_list_admin_maropost_account_path(maropost_account), class: 'button builder_action', method: :post do
+      '<span>Refresh Aweber Account</span> <span class="loader">
+            <svg height="10px" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" version="1.1">
+              <path d="M 150,0 a 150,150 0 0,1 106.066,256.066 l -35.355,-35.355 a -100,-100 0 0,0 -70.711,-170.711 z" fill="#ffffff">
+                <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 150 150" to="360 150 150" begin="0s" dur=".5s" fill="freeze" repeatCount="indefinite"></animateTransform>
+              </path>
+            </svg>
+        </span>'.html_safe
+    end
   end
 end
