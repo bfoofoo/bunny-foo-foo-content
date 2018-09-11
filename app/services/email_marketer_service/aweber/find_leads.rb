907490d7a18373f392bf3bae589a0d11c1ee8e65
Egor Romanov <unsaidxpl@gmail.com>
2018-09-11 16:06:52 +0300

    enhance find_leads logic

--- app/services/email_marketer_service/aweber/find_leads.rb
+++ app/services/email_marketer_service/aweber/find_leads.rb
@@ -2,7 +2,7 @@ module EmailMarketerService
   module Aweber
     class FindLeads
       EVENT_TYPES = %w(open click).freeze
-      DEFAULT_DATE = Date.new(2018, 8, 30) # Date since when statistic collection began
+      DEFAULT_DATE = Date.new(2018, 8, 1) # Date since when statistic collection began
 
       def initialize(since: nil)
         @auth_services = {}
@@ -13,27 +13,32 @@ module EmailMarketerService
       end
 
       def call
-        Leads::Aweber.import(collect_leads)
+        collect_leads
       end
 
       private
 
       def users
-        @users ||= User.added_to_aweber.includes(:aweber_list).includes(:leads)
+        @users ||=
+          User
+            .added_to_aweber
+            .joins(:formsite_users)
+            .includes(:aweber_list, :leads, :formsite_users)
+            .where.not(formsite_users: { affiliate: nil })
       end
 
       def collect_leads
-        leads = []
         users.each do |user|
           begin
             subscriber = subscribers_for(user.aweber_list).search('email' => user.email)&.entries&.values&.first
-            next if subscriber.nil? || !subscriber.custom_fields['Affiliate']
+            affiliate = subscriber&.custom_fields&.[]('Affiliate') || user.formsite_users.first.affiliate
+            next if subscriber.nil? || !affiliate
             subscriber.activity.each_page do |activity|
               values = activity&.entries&.values.to_a
               events = values.select { |a| a.event_time.to_date >= @since && a.type.in?(EVENT_TYPES) }
 
               events.each do |event|
-                leads << build_lead(user, subscriber, event)
+                Leads::Aweber.find_or_create_by(build_lead(user, subscriber, event))
               end
               break if (values.present? && values.last.event_time.to_date < @since)
             end
@@ -41,16 +46,15 @@ module EmailMarketerService
             puts "Aweber failed due to error: #{e.to_s}"
           end
         end
-        leads
       end
 
       def build_lead(user, subscriber, event)
         campaign = event.campaign
-        return if user.leads.any? { |l| l.type == event.type && l.event_at == campaign&.sent_at && l.details['campaign_id'] == campaign&.id }
+        affiliate = subscriber.custom_fields['Affiliate'] || user.formsite_users.first.affiliate
         {
           source_id: user.aweber_list.id,
           email: user.email,
-          affiliate: subscriber.custom_fields['Affiliate'],
+          affiliate: affiliate,
           status: event.type,
           user_id: user.id,
           event_at: campaign&.sent_at,
