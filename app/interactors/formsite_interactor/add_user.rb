a415802d23b9486a19ec9c2a4e4da735cba96687
Dendos <cotoha92@gmail.com>
2018-09-12 22:50:28 +0300

    refactor create_formsite_user method

--- app/interactors/formsite_interactor/add_user.rb
+++ app/interactors/formsite_interactor/add_user.rb
@@ -43,15 +43,16 @@ module FormsiteInteractor
 
       def create_formsite_user
         context.formsite_user = formsite.formsite_users.find_or_create_by(ip: request.env['REMOTE_ADDR']).tap do |formsite_user|
-          formsite_user.assign_attributes(formsite_user_params.merge({
-            user_id: formsite_user.user_id.blank? ? (user.blank? ? nil : user.id) : formsite_user.user_id,
-            is_verified: is_useragent_valid && is_impressionwise_test_success && !is_duplicate,
-            is_useragent_valid: is_useragent_valid,
-            is_impressionwise_test_success: is_impressionwise_test_success,
-            is_duplicate: is_duplicate,
-            affiliate: params[:user][:a]
-          }))
-          formsite_user.update(user_id: user.id) if !user.blank?
+          if formsite_user.user.blank?
+            formsite_user.update_attributes(formsite_user_params.merge({
+              user_id: formsite_user.user_id.blank? ? (user.blank? ? nil : user.id) : formsite_user.user_id,
+              is_verified: is_useragent_valid && is_impressionwise_test_success && !is_duplicate,
+              is_useragent_valid: is_useragent_valid,
+              is_impressionwise_test_success: is_impressionwise_test_success,
+              is_duplicate: is_duplicate,
+              affiliate: params[:user][:a]
+            }))
+          end
         end
       end
 
