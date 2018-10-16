cbe73232c413f6b0336ae04271e3a021e3118a4d
Egor Romanov <unsaidxpl@gmail.com>
2018-10-16 16:43:57 +0300

    auto-fetch elite esp groups

--- app/models/elite_account.rb
+++ app/models/elite_account.rb
@@ -1,13 +1,14 @@
 class EliteAccount < ApplicationRecord
-  has_many :elite_groups
+  has_many :elite_groups, dependent: :destroy
 
   validates :api_key, uniqueness: true
 
+  alias_attribute :groups, :elite_groups
   alias_attribute :lists, :elite_groups
 
-  after_create :fetch_lists
+  after_create :fetch_groups
 
-  def fetch_lists
-    # TODO implement
+  def fetch_groups
+    EmailMarketerService::Elite::FetchGroups.new(account: self).call
   end
 end
