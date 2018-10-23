4c475454e475f43dbab4c80e39f8f109bc3d13d5
Dendos <cotoha92@gmail.com>
2018-10-23 20:27:02 +0300

    hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  # ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self)
 
   root :to => redirect('/admin')
   resources :apidocs, only: [:index]
