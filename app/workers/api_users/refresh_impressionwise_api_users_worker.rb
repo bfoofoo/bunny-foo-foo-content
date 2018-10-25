b5d6070fd6233aacae4b8fdb089a18356d6fa091
Dendos <cotoha92@gmail.com>
2018-10-25 15:52:24 +0300

    create RefreshImpressionwiseApiUsersWorker

---
+++ app/workers/api_users/refresh_impressionwise_api_users_worker.rb
@@ -0,0 +1,33 @@
+module ApiUsers
+  class RefreshImpressionwiseApiUsersWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: 'api_users_refresh'
+
+    def perform(api_users_ids)
+      api_users(api_users_ids).each do |api_user|
+        is_impressionwise_test_success = is_impressionwise_test_success?(api_user)
+        is_verified = api_user.is_useragent_valid && is_impressionwise_test_success && !api_user.is_duplicate
+        api_user.update({
+          is_impressionwise_test_success: is_impressionwise_test_success,
+          is_verified: is_verified
+        })
+      end
+    end
+
+    private
+
+    def api_users(ids)
+      @api_users ||= ApiUser.where(id: ids)
+    end
+
+    def formsite_service
+      @formsite_service ||= FormsiteService.new
+    end
+
+    def is_impressionwise_test_success?(user)
+      return false if user.blank?
+      formsite_service.is_impressionwise_test_success(user)
+    end
+
+  end
+end
