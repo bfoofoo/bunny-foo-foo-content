f4106a33ffbbfc6cf1bd03cda12330b9b4aad3a9
Dendos <cotoha92@gmail.com>
2018-10-31 22:37:15 +0300

    add account and list to subscription service

--- app/services/email_marketer_service/netatlantic/subscription_service.rb
+++ app/services/email_marketer_service/netatlantic/subscription_service.rb
@@ -1,17 +1,18 @@
 module EmailMarketerService
   module Netatlantic
     class SubscriptionService < EmailMarketerService::Netatlantic::BaseService
-      attr_reader :list, :params
+      attr_reader :list, :params, :account
 
       def initialize(list, params: nil, esp_rule: nil)
         @list = list
         @params = params
+        @account = @list.netatlantic_account
         @esp_rule = esp_rule
       end
 
       def add_subscriber(user)
         user_name = user.try(:full_name).blank? ? user.try(:name) : user.full_name
-        HTTParty.post("#{API_PATH}/create_member.php", body: {email: user.email, full_name: user_name})
+        HTTParty.post("#{API_PATH}/create_member.php", body: {email: user.email, full_name: user_name, account: account.account_name, list: list.name})
       end
     end
   end
