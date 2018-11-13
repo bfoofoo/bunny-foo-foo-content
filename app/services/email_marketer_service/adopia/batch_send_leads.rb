2fe35e29dc6f57a20732c34d78535c216af4ebc8
Egor Romanov <unsaidxpl@gmail.com>
2018-11-13 10:40:44 +0300

    enhance adopia batch lead sending

--- app/services/email_marketer_service/adopia/batch_send_leads.rb
+++ app/services/email_marketer_service/adopia/batch_send_leads.rb
@@ -1,32 +1,31 @@
 module EmailMarketerService
   module Adopia
     class BatchSendLeads
-      attr_reader :emails, :processed_emails
+      attr_reader :data, :processed_emails
 
       BATCH_SIZE = 50.freeze
 
-      def initialize(emails, account_name = nil)
+      def initialize(data, account_name = nil, list_name = nil)
         @clients = {}
-        @emails = emails
+        @data = data
         @account_name = account_name
+        @list_name = list_name
         @processed_emails = []
       end
 
       def call
         accounts.each do |account|
           client = client_for(account)
-          account.lists.each do |list|
-            emails.each do |email|
-              begin
-                client.add_list_contact(list.list_id, {
-                  contact_email: email,
+          data.each_slice(BATCH_SIZE) do |slice|
+            begin
+              client.add_list_contacts(selected_list_ids(account), {
+                  contacts: build_contacts(slice),
                   is_double_opt_in: 0
-                })
-                @processed_emails << email
-                sleep 0.6 # to send maximum 100 per minute
-              rescue ::Adopia::Errors::Error => e
-                puts "Adopia adding subscriber error - #{e}".red
-              end
+              })
+              @processed_emails << slice
+              sleep 0.6 # to send maximum 100 per minute
+            rescue ::Adopia::Errors::Error => e
+              puts "Adopia adding subscriber error - #{e}".red
             end
           end
         end
@@ -42,6 +41,17 @@ module EmailMarketerService
 
       def client_for(account)
         @clients[account.id] ||= ::Adopia::Client.new(account.api_key)
+      end
+
+      def build_contacts(slice)
+        slice.map do |item|
+          item.is_a?(Hash) ? item : { contact_email: item }
+        end
+      end
+
+      def selected_list_ids(account)
+        return account.lists.pluck(:list_id) unless @list_name
+        [account.lists.find_by(name: @list_name)&.list_id]
       end
     end
   end
