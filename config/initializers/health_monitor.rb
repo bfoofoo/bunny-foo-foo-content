620638f6eb68284a8cf331063b68fd82fd2bec55
Egor Romanov <unsaidxpl@gmail.com>
2018-11-27 13:24:49 +0300

    configure redis

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -1,6 +1,8 @@
 HealthMonitor.configure do |config|
   config.cache
-  config.redis
+  config.redis.configure do |redis_config|
+    redis_config.connection = Redis.current
+  end
   config.sidekiq.configure do |sidekiq_config|
     sidekiq_config.latency = 1.hour
   end
