958a01338e71a580f3bc8d40300e905f6c3bedcb
comeonandroid <comeonandroid@gmail.com>
2018-12-04 23:19:36 +0700

    add url to formsite users

--- app/interactors/formsite_interactor/add_user.rb
+++ app/interactors/formsite_interactor/add_user.rb
@@ -152,7 +152,7 @@ module FormsiteInteractor
       end
 
       def formsite_user_params
-        params.require(:user).permit(:user_id, :s1, :s2, :s3, :s4, :s5, :birthday, :phone, :zip, :site_type)
+        params.require(:user).permit(:user_id, :s1, :s2, :s3, :s4, :s5, :birthday, :phone, :zip, :site_type, :url)
       end
 
       def job_formsite_user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181204093657) do
+ActiveRecord::Schema.define(version: 20181204161407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -404,6 +404,7 @@ ActiveRecord::Schema.define(version: 20181204093657) do
     t.string   "data_key"
     t.string   "site_type",                      default: "leadgen"
     t.integer  "website_id"
+    t.string   "url"
     t.index ["deleted_at"], name: "index_formsite_users_on_deleted_at", using: :btree
     t.index ["website_id"], name: "index_formsite_users_on_website_id", using: :btree
   end
