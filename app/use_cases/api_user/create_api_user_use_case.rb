b19736fd0c53db2b7e59672e8295955272ede211
Egor Romanov <unsaidxpl@gmail.com>
2018-12-10 15:17:49 +0300

    Save api user state

--- app/use_cases/api_user/create_api_user_use_case.rb
+++ app/use_cases/api_user/create_api_user_use_case.rb
@@ -28,13 +28,20 @@ class ApiUser
       return {message: "success", status: 200}
     end
 
+    def user_state_for_ip(ip)
+      results = Geocoder.search(ip)
+      results&.first&.state
+    rescue
+      nil
+    end
+
     def create_api_user
-      user = build_api_user(params)
-      if user.save
-        send_to_esp(user)
+      @user = build_api_user(params)
+      if @user.save
+        send_to_esp
         return {message: "success", status: 200}
       else
-        return {message: user.errors, status: :unprocessable_entity}
+        return {message: @user.errors, status: :unprocessable_entity}
       end
     end
     
@@ -59,6 +66,7 @@ class ApiUser
       @api_user = ApiUser.new(
         parsed_params.merge(
             api_client_id: api_client.id,
+            state: user_state_for_ip(parsed_params[:ip]),
             is_verified: is_useragent_valid && is_impressionwise_test_success(parsed_params) && !is_duplicate(parsed_params[:email]),
             is_useragent_valid: is_useragent_valid,
             is_impressionwise_test_success: is_impressionwise_test_success(parsed_params),
@@ -67,8 +75,8 @@ class ApiUser
       )
     end
 
-    def send_to_esp(user)
-      ApiUser::AddNewUserToEspUseCase.new(user).perform
+    def send_to_esp
+      ApiUser::AddNewUserToEspUseCase.new(@user).perform
     end
 
     def api_user_params(user_params)
