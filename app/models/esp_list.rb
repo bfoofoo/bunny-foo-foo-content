97dc1efc12e7b25f8e36b7eb381c7075c7e1a98a
Egor Romanov <unsaidxpl@gmail.com>
2018-12-12 10:26:56 +0300

    Fix ESP lists

--- app/models/esp_list.rb
+++ app/models/esp_list.rb
@@ -1,6 +1,8 @@
 class EspList < ApplicationRecord
   has_many :exported_leads, as: :list
 
+  before_destroy :purge_esp_rules_lists
+
   delegate :sending_limit, to: :account
 
   def id_with_type
@@ -10,5 +12,11 @@ class EspList < ApplicationRecord
   def full_name
     return name if account.display_name.blank?
     "#{account.display_name} - #{name}"
+  end
+
+  # Workaround to delete esp rules lists
+  # TODO resolve this to work in rails way
+  def purge_esp_rules_lists
+    EspRulesList.where(list_type: type, list_id: id).delete_all
   end
 end
