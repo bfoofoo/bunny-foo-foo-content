b19f26b66a33fdb7ab22ea7a120ccaa7f8f793f4
Egor Romanov <unsaidxpl@gmail.com>
2018-12-13 15:22:19 +0300

    hotfix: Message migration

--- db/migrate/20181213115641_migrate_mailgun_templates.rb
+++ db/migrate/20181213115641_migrate_mailgun_templates.rb
@@ -9,7 +9,7 @@ class MigrateMailgunTemplates < ActiveRecord::Migration[5.0]
         esp_list_id: mts.mailgun_list_id,
         eps_list_type: 'MailgunList',
         time: mts.sending_time,
-        message_template_id: MessageTemplate.find_by(subject: mts.subject)&.id,
+        message_template_id: MessageTemplate.find_by(subject: mts.mailgun_template&.subject)&.id,
         scheduled_job_id: mts.scheduled_job_id
       )
     end
