8eeb5a62db56912140e75c584e68ae97d67c4bbb
t43riz <t43riz@users.noreply.github.com>
2018-12-18 12:06:32 -0600

    Update constantcontact_list.rb

--- app/models/esp_lists/constantcontact_list.rb
+++ app/models/esp_lists/constantcontact_list.rb
@@ -1,4 +1,4 @@
 class ConstantContactList < EspList
   belongs_to :constantcontact_account, foreign_key: :account_id
-  alias_attribute :account, :allinbox_account
+  alias_attribute :account, :constantcontact_account
 end
