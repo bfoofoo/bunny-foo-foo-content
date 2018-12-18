37a4ca4781f6fff5302e5fdccebd8c2bbde89a08
Riz <inventorriz@gmail.com>
2018-12-18 12:30:47 -0600

    fix spelling

--- app/models/esp_lists/constantcontact_list.rb
+++ app/models/esp_lists/constantcontact_list.rb
@@ -1,4 +1,4 @@
-class ConstantContactList < EspList
+class ConstantcontactList < EspList
   belongs_to :constantcontact_account, foreign_key: :account_id
   alias_attribute :account, :constantcontact_account
 end
