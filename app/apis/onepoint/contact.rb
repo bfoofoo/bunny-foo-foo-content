9595eac1bc35a5fbb6ed1563b6a73f53e3b513db
Egor Romanov <unsaidxpl@gmail.com>
2018-12-19 08:57:43 +0300

    Add retry to 1point

--- app/apis/onepoint/contact.rb
+++ app/apis/onepoint/contact.rb
@@ -1,8 +1,12 @@
 module Onepoint
   class Contact < Resource
     def create(params)
+      retries ||= 0
       response = @request.post(nil, params.compact)
       Response.new(response).parse
+    rescue Net::ReadTimeout
+      retries += 1
+      retry if retries < 3
     end
 
     private
