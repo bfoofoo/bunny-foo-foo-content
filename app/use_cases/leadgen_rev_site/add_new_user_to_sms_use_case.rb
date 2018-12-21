8be061970814c3ea4ab3e695a898663426487bbd
dendos <cotoha92@gmail.com>
2018-12-21 19:05:05 +0700

    create AddNewUserToSMSUseCase

---
+++ app/use_cases/leadgen_rev_site/add_new_user_to_sms_use_case.rb
@@ -0,0 +1,26 @@
+class LeadgenRevSite
+  class AddNewUserToSMSUseCase
+
+    attr_reader :leadgen_rev_site, :user, :leadgen_rev_site_user, :params
+
+    def initialize(leadgen_rev_site, user, leadgen_rev_site_user)
+      @user = user
+      @leadgen_rev_site = leadgen_rev_site
+      @leadgen_rev_site_user = leadgen_rev_site_user
+      @params = {
+        affiliate: leadgen_rev_site_user.affiliate,
+        ipAddress: leadgen_rev_site_user.ip,
+        url: leadgen_rev_site_user.url,
+        ip: leadgen_rev_site_user.ip,
+        date: leadgen_rev_site_user.created_at,
+        signup_method: 'Webform',
+        state: leadgen_rev_site_user.state
+      }.compact
+    end
+
+    def perform
+      return false if !leadgen_rev_site_user.is_verified || user.blank?
+      Sms::Waypoint::SubscriptionService.new(params).add(user)
+    end
+  end
+end
