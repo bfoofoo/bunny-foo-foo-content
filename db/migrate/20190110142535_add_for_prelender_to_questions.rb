fd5a64838199f5d0ecd36e0aff532b3f7a23d607
Egor Romanov <unsaidxpl@gmail.com>
2019-01-11 12:14:49 +0300

    Fix migration typo

--- db/migrate/20190110142535_add_for_prelender_to_questions.rb
+++ db/migrate/20190110142535_add_for_prelender_to_questions.rb
@@ -1,5 +1,5 @@
 class AddForPrelenderToQuestions < ActiveRecord::Migration[5.0]
   def change
-    add_column :questions, :for_prelender, :boolean, default: fals
+    add_column :questions, :for_prelender, :boolean, default: false
   end
 end
