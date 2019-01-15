557b5d3e10fb4329361805727e7bb39a265b2814
Egor Romanov <unsaidxpl@gmail.com>
2019-01-15 16:44:31 +0300

    Schedule colossus sending

--- app/use_cases/leadgen_rev_site/add_new_user_to_esp_use_case.rb
+++ app/use_cases/leadgen_rev_site/add_new_user_to_esp_use_case.rb
@@ -15,12 +15,14 @@ class LeadgenRevSite
         ip: leadgen_rev_site_user.ip,
         date: leadgen_rev_site_user.created_at,
         signup_method: 'Webform',
-        state: leadgen_rev_site_user.state
+        state: leadgen_rev_site_user.state,
+        phone: leadgen_rev_site_user.phone
       }.compact
     end
 
     def perform
       return false if !leadgen_rev_site_user.is_verified || user.blank?
+      schedule_for_colossus
       rules.each do |rule|
         next if rule.domain.present? && !(leadgen_rev_site_user.email =~ /@#{Regexp.quote(rule.domain)}\.\w+$/)
         if rule.split?
@@ -35,6 +37,10 @@ class LeadgenRevSite
 
     def rules
       EspRules::LeadgenRevSite.where(source: leadgen_rev_site, delay_in_hours: 0)
+    end
+
+    def schedule_for_colossus
+      LeadgenRevSiteUsers::SendToColossusWorker.perform_at(10.minutes.from_now, leadgen_rev_site_user.id)
     end
   end
 end
