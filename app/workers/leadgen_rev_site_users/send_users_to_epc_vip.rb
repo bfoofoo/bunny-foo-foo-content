64d94ea0f1bcaa379ae6fd94e81ba51dbe351608
Egor Romanov <unsaidxpl@gmail.com>
2019-01-25 14:31:07 +0300

    Hotfix: update epcvip schedule

--- app/workers/leadgen_rev_site_users/send_users_to_epc_vip.rb
+++ app/workers/leadgen_rev_site_users/send_users_to_epc_vip.rb
@@ -5,6 +5,7 @@ module LeadgenRevSiteUsers
     def perform
       hour = Time.zone.now.beginning_of_hour
       leadrev_users = LeadgenRevSiteUser.is_verified.joins(:user).where(created_at: hour - 8.days..hour - 7.days)
+      leadrev_users = leadrev_users.to_a.reject(&:sent_to_epcvip)
       leadrev_users.each do |lrsu|
         return unless lrsu.user
         leadgen_rev_site = LeadgenRevSite.find(lrsu.leadgen_rev_site_id)
