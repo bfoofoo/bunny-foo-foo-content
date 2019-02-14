210ba99c27317e9fb52fa9e51c7c5ee44c76a94c
Egor Romanov <unsaidxpl@gmail.com>
2019-02-14 23:04:29 +0300

    Fix ConstantContact subscription service

--- app/services/email_marketer_service/constantcontact/subscription_service.rb
+++ app/services/email_marketer_service/constantcontact/subscription_service.rb
@@ -10,22 +10,19 @@ module EmailMarketerService
       end
 
       def add(user)
-        begin
-          if is_valid?(user)
-            lid =  list.list_id
-            rlist = client.get_list(lid).id
-            new_contact = ConstantContact::Components::Contact.new
-            new_contact.add_email(ConstantContact::Components::EmailAddress.new(user.email))
-            list_to_add_to = ConstantContact::Components::ContactList.new
-            list_to_add_to.id = rlist
-            new_contact.add_list(list_to_add_to)
-            new_contact.first_name = user.try(:first_name)
-            new_contact.last_name = user.try(:last_name)
-            handle_user_record(user)
-          end
-           rescue RestClient::BadRequest => e
-             puts "#{e.http_code} - #{e.http_body}"
+        if is_valid?(user)
+          new_contact = ConstantContact::Components::Contact.new
+          new_contact.add_email(ConstantContact::Components::EmailAddress.new(user.email))
+          list_to_add_to = ConstantContact::Components::ContactList.new
+          list_to_add_to.id = list.list_id
+          new_contact.add_list(list_to_add_to)
+          new_contact.first_name = user.try(:first_name)
+          new_contact.last_name = user.try(:last_name)
+          client.add_contact(new_contact)
+          handle_user_record(user)
         end
+      rescue RestClient::BadRequest => e
+        puts "#{e.http_code} - #{e.http_body}"
       end
 
       private
@@ -46,11 +43,10 @@ module EmailMarketerService
         return @client if defined?(@client)
         @client = ConstantContact::Api.new(list.account.api_key, list.account.access_token)
       end
-      
+
       def account
         @account ||= list.constantcontact_account
       end
-
     end
   end
 end
